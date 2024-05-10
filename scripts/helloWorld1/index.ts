520f34c192e8f045127a3542116bb619f340c962
Lutz <meteo_meteo@naver.com>
2024-05-10 13:11:09 +0900

    feat: 테스트용 스크립트 추가

---
+++ scripts/helloWorld1/index.ts
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Hello, World! 1
+// @namespace    https://aerocode.net
+// @version      1.0.0
+// @description  try to take over the world!
+// @author       Lutz
+// @match        https://*/*
+// @run-at       document-start
+// @icon         <$ICON$>
+// @grant        none
+// ==/UserScript==
+
+async function main() {
+  console.log("Hello, World! 1");
+}
+
+main();
